e7bf85ba345dea49dcaf9ec6cd24729a654180a0
carter.fendley <carter.fendley@gmail.com>
2017-01-29 16:45:09 -0500

    Starts to implement rotational motor sim

--- robot/physics.py
+++ robot/physics.py
@@ -1,7 +1,6 @@
 import math
 
 from networktables.util import ntproperty
-from pyfrc.physics.drivetrains import four_motor_drivetrain
 import wpilib
 import ctre
 
@@ -13,6 +12,14 @@ class PhysicsEngine:
 
         self.controller.add_device_gyro_channel('navxmxp_spi_4_angle')
 
+
+
+    def initialize(self, hal_data):
+        self.rr_rotate_encoder = 2
+        self.rl_rotate_encoder = 3
+        self.fr_rotate_encoder = 4
+        self.fl_rotate_encoder = 1
+
     """
         Update pyfrc simulator
 
@@ -22,6 +29,35 @@ class PhysicsEngine:
         now -- Current time in ms
         tm_diff -- Diff between current time and time when last checked
     """
-
     def update_sim(self, hal_data, now, tm_diff):
-        pass
+        
+
+        try:
+            self.rr_rotate_encoder += -hal_data['pwm'][1]['value'] * tm_diff * 1
+            self.rl_rotate_encoder += -hal_data['pwm'][2]['value'] * tm_diff * 1
+            self.fr_rotate_encoder += -hal_data['pwm'][3]['value'] * tm_diff * 1
+            self.fl_rotate_encoder += -hal_data['pwm'][4]['value'] * tm_diff * 1
+            
+            self.rr_rotate_encoder %= 5
+            self.rl_rotate_encoder %= 5
+            self.fr_rotate_encoder %= 5
+            self.fl_rotate_encoder %= 5
+            
+            hal_data['analog_in'][1]['voltage'] = self.rr_rotate_encoder
+            hal_data['analog_in'][2]['voltage'] = self.rl_rotate_encoder
+            hal_data['analog_in'][3]['voltage'] = self.fr_rotate_encoder
+            hal_data['analog_in'][4]['voltage'] = self.fl_rotate_encoder
+            #print('---\nRR_ENC:%s\n' % hal_data['analog_in'][1]['voltage'])
+        except:
+            pass
+
+        try:
+            rr_motor = -hal_data['CAN'][10]['value'] / 1023
+            rl_motor = -hal_data['CAN'][15]['value'] / 1023
+            fr_motor = -hal_data['CAN'][20]['value'] / 1023
+            fl_motor = -hal_data['CAN'][25]['value'] / 1023
+
+
+        except:
+            pass
+
