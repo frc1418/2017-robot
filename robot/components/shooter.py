17129fe766f2e3837a0e8573bb4b326af803880f
ErikBoesen <erik.boesen@gmail.com>
2017-02-20 13:00:21 -0500

    Move component set()ting to execute

--- robot/components/shooter.py
+++ robot/components/shooter.py
@@ -6,30 +6,35 @@ from magicbot.magic_tunable import tunable
 class Shooter(StateMachine):
     belt_motor = wpilib.spark.Spark
     shooter_motor = ctre.CANTalon
-    
-    shooter_speed = tunable(-0.9)
-        
+
+    belt_speed = 0
+    shooter_speed = 0
+    max_shooter_speed = tunable(-0.9)
+
     def stop(self):
         self.shooter_motor.set(0)
         self.belt_motor.set(0)
-        
+
         self.done()
-    
+
     def shoot(self):
         self.engage()
-        
+
     def force_spin(self):
-        self.shooter_motor.set(self.shooter_speed)
-    
+        self.shooter_speed = self.max_shooter_speed
+
     def force_feed(self):
-        self.belt_motor.set(-1)
-    
+        self.belt_speed = -1
+
     @timed_state(duration=1.0, first=True, next_state='feed_shoot')
     def spinup(self):
-        self.shooter_motor.set(self.shooter_speed)
-    
-    @state  
+        self.shooter_speed = self.max_shooter_speed
+
+    @state
     def feed_shoot(self):
+        self.shooter_speed = self.max_shooter_speed
+        self.belt_speed = -1
+
+    def execute(self):
+        self.belt_motor.set(self.belt_speed)
         self.shooter_motor.set(self.shooter_speed)
-        self.belt_motor.set(-1)
-        
