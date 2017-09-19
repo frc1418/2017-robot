259f20ba3b1cbdcbe27ef047c73a366311ad06de
ErikBoesen <me@erikboesen.com>
2017-09-18 22:42:13 -0400

    Clean up math and syntax

--- robot/components/swervemodule.py
+++ robot/components/swervemodule.py
@@ -2,12 +2,9 @@ import wpilib
 import math
 
 from networktables import NetworkTable
-from networktables.util import ntproperty
-
 
 MAX_VOLTAGE = 5
 MAX_TICK = 4096
-MAX_DEG = 360
 
 WHEEL_DIAMETER = 4/12  # 4 Inches
 WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * math.pi
@@ -16,56 +13,50 @@ WHEEL_TICKS_PER_REV = 55000
 
 class SwerveModule:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, drive_motor, rotate_motor, encoder, sd_prefix='SwerveModule', inverted=False, zero=0.0, allow_reverse=True, has_drive_encoder=False):
         """
         Swerve drive module was written for a swerve drive train that uses absolute encoders for tracking wheel rotation.
 
-        When positional arguments are used:
-            :param driveMotor: Motor object
-            :param rotateMotor: Motor object
-            :param encoder: AnalogInput wpilib object
+        :param drive_motor: Motor object
+        :param rotate_motor: Motor object
+        :param encoder: AnalogInput wpilib object
 
-        When key word arguments are used:
-            :param encoderPort: analog in port number of Absolute Encoder
+        :param sd_prefix: a string used to differentiate modules when debugging
+        :param zero: The default zero for the encoder
+        :param inverted: boolean to invert the wheel rotation
 
-            :param SDPrefix: a string used to differentiate modules when debugging
-            :param zero: The default zero for the encoder
-            :param inverted: boolean to invert the wheel rotation
-
-            :param allow_reverse: If true allows wheels to spin backwards instead of rotating
-            :param has_drive_encoder: If true allows the module to track wheel position
-
+        :param allow_reverse: If true allows wheels to spin backwards instead of rotating
+        :param has_drive_encoder: If true allows the module to track wheel position
         """
-
         # SmartDashboard
         self.sd = NetworkTable.getTable('SmartDashboard')
-        self.sd_prefix = kwargs.pop('SDPrefix', False)
+        self.sd_prefix = sd_prefix
 
         # Motors
-        self.driveMotor = args[0]
-        self.drive_inverted = kwargs.pop('inverted', False)
-        self.driveMotor.setInverted(self.drive_inverted)
+        self.drive_motor = drive_motor
+        self.drive_inverted = inverted
+        self.drive_motor.setInverted(self.drive_inverted)
 
-        self.rotateMotor = args[1]
+        self.rotate_motor = rotate_motor
 
         self._requested_voltage = 0  # Angle in voltage form
         self._requested_speed = 0
 
         # Encoder
-        self.encoder = args[2]
-        self.encoder_zero = kwargs.pop('zero', 0.0)
+        self.encoder = encoder
+        self.encoder_zero = zero
 
         # PID
-        self._pid_controller = wpilib.PIDController(1.5, 0.0, 0.0, self.encoder, self.rotateMotor)
+        self._pid_controller = wpilib.PIDController(1.5, 0.0, 0.0, self.encoder, self.rotate_motor)
         self._pid_controller.setContinuous()
         self._pid_controller.setInputRange(0.0, 5.0)
         self._pid_controller.enable()
 
         # State variables
-        self.allow_reverse = kwargs.pop('allow_reverse', True)
+        self.allow_reverse = allow_reverse
         self.debugging = self.sd.getAutoUpdateValue('drive/%s/debugging' % self.sd_prefix, False)
 
-        self.has_drive_encoder = kwargs.pop('has_drive_encoder', False)
+        self.has_drive_encoder = has_drive_encoder
 
     def set_pid(self, p, i, d):
         self._pid_controller.setPID(p, i, d)
@@ -78,12 +69,12 @@ class SwerveModule:
 
     def get_drive_encoder_tick(self):
         """
-        :return: the positional value of the drive encoder
+        :return: the positional value of the drive encoder.
         """
         if not self.has_drive_encoder:
             return False
 
-        return self.driveMotor.getPosition()
+        return self.drive_motor.getPosition()
 
     def get_drive_encoder_distance(self):
         """
@@ -92,11 +83,11 @@ class SwerveModule:
         if not self.has_drive_encoder:
             return False
 
-        return ((self.driveMotor.getPosition()) * WHEEL_CIRCUMFERENCE) / WHEEL_TICKS_PER_REV
+        return ((self.drive_motor.getPosition()) * WHEEL_CIRCUMFERENCE) / WHEEL_TICKS_PER_REV
 
     def flush(self):
         """
-        Flushes the modules requested speed and voltage
+        Flush the modules requested speed and voltage.
         """
         self._requested_voltage = self.encoder.getVoltage()
         self._requested_speed = 0
@@ -104,94 +95,86 @@ class SwerveModule:
     @staticmethod
     def voltage_to_degrees(voltage):
         """
-        Convert a given voltage value to degrees
+        Convert a given voltage value to degrees.
 
         :param voltage: a voltage value between 0 and 5
         """
-
         deg = (voltage/5)*360
         deg = deg
 
         if deg < 0:
-            deg = 360+deg;
+            deg = 360+deg
 
         return deg
 
     @staticmethod
     def voltage_to_rad(voltage):
         """
-        Convert a given voltage value to rad
+        Convert a given voltage value to rad.
 
         :param voltage: a voltage value between 0 and 5
         """
-
         rad = (voltage/5)*2*math.pi
         return rad
 
     @staticmethod
     def voltage_to_tick(voltage):
         """
-        Convert a given voltage value to tick
+        Convert a given voltage value to tick.
 
         :param voltage: a voltage value between 0 and 5
         """
-
         return (voltage/5)*4050
 
     @staticmethod
     def degree_to_voltage(degree):
         """
-        Convert a given degree to voltage
+        Convert a given degree to voltage.
 
         :param degree: a degree value between 0 and 360
         """
-
         return (degree/360)*5
 
     @staticmethod
     def tick_to_voltage(tick):
         """
-        Convert a given tick to voltage
+        Convert a given tick to voltage.
 
         :param tick: a tick value between 0 and 4050
         """
-
         return (tick/4050)*5
 
     def zero_encoder(self):
         """
-        Set the zero to the current voltage output
+        Set the zero to the current voltage output.
         """
 
         self.encoder_zero = self.encoder.getVoltage()
 
     def is_aligned(self):
         """
-        :return: True if wheel is aligned to set point (False if not)
+        :return: Whether wheel is aligned to set point
         """
-        if abs(self._pid_controller.getError()) < 0.1:
-            return True
-        return False
+        return abs(self._pid_controller.getError()) < 0.1
 
     def _set_deg(self, value):
         """
         Round the value to within 360. Set the requested rotate position (requested voltage).
         Intended to be used only by the move function.
         """
-
         self._requested_voltage = ((self.degree_to_voltage(value)+self.encoder_zero) % 5)
 
     def move(self, speed, deg):
         """
         Set the requested speed and rotation of passed.
-        
+
         :param speed: requested speed of wheel from -1 to 1
         :param deg: requested angle of wheel from 0 to 359 (Will wrap if over or under)
         """
         deg %= 360  # Prevent values past 360
 
         if self.allow_reverse:
-            if abs( deg - self.voltage_to_degrees(self.get_voltage()) ) > 90:
+            if abs(deg - self.voltage_to_degrees(self.get_voltage())) > 90:
                 speed *= -1
                 deg += 180
 
@@ -202,7 +185,7 @@ class SwerveModule:
 
     def debug(self):
         """
-        Prints debugging information about the module to the log.
+        Print debugging information about the module to the log.
         """
         print(self.sd_prefix, '; requested_speed: ', self._requested_speed, ' requested_voltage: ', self._requested_voltage)
 
@@ -213,9 +196,8 @@ class SwerveModule:
 
         Should be called every robot iteration/loop.
         """
-
         self._pid_controller.setSetpoint(self._requested_voltage)
-        self.driveMotor.set(self._requested_speed)
+        self.drive_motor.set(self._requested_speed)
 
         self._requested_speed = 0.0
 
@@ -225,11 +207,10 @@ class SwerveModule:
         """
         Output a bunch on internal variables for debugging purposes.
         """
-
         self.sd.putNumber('drive/%s/degrees' % self.sd_prefix, self.voltage_to_degrees(self.get_voltage()))
 
         if self.has_drive_encoder:
-            self.sd.putNumber('drive/%s/raw drive position' % self.sd_prefix, self.driveMotor.getPosition())
+            self.sd.putNumber('drive/%s/raw drive position' % self.sd_prefix, self.drive_motor.getPosition())
 
         if self.debugging.value:
             self.sd.putNumber('drive/%s/requested_voltage' % self.sd_prefix, self._requested_voltage)
--- robot/controllers/angle_controller.py
+++ robot/controllers/angle_controller.py
@@ -1,8 +1,3 @@
-
-import math
-
-import hal
-
 from magicbot import tunable
 from robotpy_ext.common_drivers import navx
 
@@ -48,7 +43,7 @@ class AngleController(BasePIDComponent):
     def align_to(self, angle):
         """
         Move the robot and turns it to a specified absolute direction.
-        
+
         :param angle: Angle value from 0 to 359
         """
         self.setpoint = angle
@@ -62,23 +57,12 @@ class AngleController(BasePIDComponent):
         if not self.enabled:
             return False
 
-        angle = self.get_angle()
-        setpoint = self.setpoint
-
-        # compensate for wraparound (code from PIDController)
-        error = setpoint - angle
-        if abs(error) > 180.0:
-            if error > 0:
-                error = error - 360.0
-            else:
-                error = error + 360.0
-
-        return abs(error) < self.kToleranceDegrees
+        return self.is_aligned_to(self.setpoint)
 
     def is_aligned_to(self, setpoint):
         """
         :param setpoint: Setpoint value to check alignment against
-        
+
         :return: True if aligned False if not
         """
         angle = self.get_angle()
@@ -87,9 +71,9 @@ class AngleController(BasePIDComponent):
         error = setpoint - angle
         if abs(error) > 180.0:
             if error > 0:
-                error = error - 360.0
+                error -= 360.0
             else:
-                error = error + 360.0
+                error += 360.0
 
         return abs(error) < self.kToleranceDegrees
 
@@ -98,18 +82,17 @@ class AngleController(BasePIDComponent):
 
     def compute_error(self, setpoint, pid_input):
         """
-        Computes the error between the setpoint and pis_input
-        
+        Compute the error between the setpoint and pid_input.
+
         :return: A value in degrees of error. (360 to -360)
         """
-        
         error = pid_input - setpoint
-        
-        if abs(error) > 180.0: # Used to find the closest path to the setpoint
+
+        if abs(error) > 180.0:  # Used to find the closest path to the setpoint
             if error > 0:
-                error = error - 360.0
+                error -= 360.0
             else:
-                error = error + 360.0
+                error += 360.0
 
         return error
 
@@ -121,15 +104,14 @@ class AngleController(BasePIDComponent):
             if self.is_aligned():
                 self.drive.set_raw_rcw(0)
             else:
-                #print('Setting rotation: %s' % self.rate)
                 self.drive.set_raw_rcw(self.rate)
 
 
 class MovingAngleController(AngleController):
     """
-    This class is a hacky way of changing the PID vraiables when the robot is moving. (Less friction)
+    This class is a hacky way of changing the PID vraiables when the robot is moving (Less friction).
     """
-    
+
     kP = tunable(0.002)
     kI = tunable(0.0)
     kD = tunable(0.0)
